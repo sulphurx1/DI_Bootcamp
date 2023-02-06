e7fd8dfe31bb007486db802c6afb2862259f677f
sulphurx1 <122863442+sulphurx1@users.noreply.github.com>
2023-02-06 15:45:01 +0400

    corrrect

--- Week4/Day1/DailyChallenge/main.py
+++ Week4/Day1/DailyChallenge/main.py
@@ -9,3 +9,9 @@ else:
     #     print(char)
     for i in range(len(answer)):
         print(answer[:i + 1])
+
+import random
+new_answer = list(answer)
+random.shuffle(new_answer)
+new_answer = ''.join(new_answer)
+print(new_answer)
